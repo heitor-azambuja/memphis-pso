7e9ad02e60280a90b85c004c134fc83414899fbc
azambuja_heitor <heitortazamba@gmail.com>
2022-08-04 11:23:09 -0300

    Optimizes rosenbrock function

--- sequential-pso/pso.c
+++ sequential-pso/pso.c
@@ -1,36 +1,45 @@
 #include <api.h>  // Memphis parallel platform api
 #include <stdlib.h>  // Memphis version o stdlib, for rand()
 
-int fitness(int x, int y, int z) {
+// int fitness(int x, int y, int z) {
 
-    int penalty = 10000;
+//     int penalty = 10000;
 
-    int objective_function = 10 * (x - 1) * (x - 1) + 20 * (y - 2) * (y - 2) + 30 * (z - 3) * (z - 3);
+//     int objective_function = 10 * (x - 1) * (x - 1) + 20 * (y - 2) * (y - 2) + 30 * (z - 3) * (z - 3);
 
-    // Constrains (all constrains must be converted into <=0)
-    int constrains[2];
+//     // Constrains (all constrains must be converted into <=0)
+//     int constrains[2];
     
-    constrains[0] = x + y + z - 5;
-    constrains[1] = x * x + 2 * y - z;
+//     constrains[0] = x + y + z - 5;
+//     constrains[1] = x * x + 2 * y - z;
 
-    // defining penalty for each constrain
-    int count = sizeof(constrains);
-    int constrain_sum = 0;
-    for (int i = 0; i < count; i++) {
-        if (constrains[i] > 0) {
-            constrain_sum++;
-        }
-    }
+//     // defining penalty for each constrain
+//     int count = sizeof(constrains);
+//     int constrain_sum = 0;
+//     for (int i = 0; i < count; i++) {
+//         if (constrains[i] > 0) {
+//             constrain_sum++;
+//         }
+//     }
 
-    return objective_function + penalty * constrain_sum;
+//     return objective_function + penalty * constrain_sum;
+// }
+
+int fitness(int x, int y, int z) {
+    // rosenbrock function
+    int x_factor = 100 * (y - (x * x)) * (y - (x * x)) + (1 - x) * (1 - x);
+    int y_factor = 100 * (z - (y * y)) * (z - (y * y)) + (1 - y) * (1 - y);
+    return x_factor + y_factor;
 }
 
 void main(){
 
     int SEED = 135430;  // for rand() function
 
-    int lower_bounds[3] = {0, 0, 0};
-    int upper_bounds[3] = {10, 10, 10};
+    // int lower_bounds[3] = {0, 0, 0};
+    // int upper_bounds[3] = {10, 10, 10};
+    int lower_bounds[3] = {-100, -100, -100};
+    int upper_bounds[3] = {100, 100, 100};
 
     // PSO parameters
     int dimensions = 3;
@@ -39,8 +48,8 @@ void main(){
     // float damping_factor = 0.99; // Memphis supposedely doesn't support floats
     int cognitive_weight = 2;
     int social_weight = 2;
-    int min_velocity = -1;
-    int max_velocity = 1;
+    int min_velocity = -2;
+    int max_velocity = 2;
     int error_tolerance = 10;
     int max_iterations = 200;
 
